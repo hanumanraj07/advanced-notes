22c9d597b28b61371f5b83f8f9bf7779da853937
Hanuman Singh <hanuman.rajpurohit.cg@gmail.com>
2026-04-23 23:29:30 +0530

    feat: add routes and server setup

---
+++ src/app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const connectDB = require('./config/db');
+
+dotenv.config();
+connectDB();
+
+const app = express();
+app.use(express.json());
+app.use('/api/notes', require('./routes/note.routes'));
+
+module.exports = app;
